5f0df2a07d66c0ce535c7a6c1d1416600718c4b8
Ahammed Ussain <habibhassan8489@gmail.com>
2025-08-20 22:09:14 +0530

    second commit

--- app/app.py
+++ app/app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello, Flask with Docker & Jenkins hi guys!"
+    return "Hello, Flask with Docker & Jenkins hello guys!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
